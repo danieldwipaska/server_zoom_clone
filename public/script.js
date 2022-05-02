1bea620b62efd5634a6aec10fdd7295736e503a0
danieldwipaska <scientificallychristian@gmail.com>
2022-05-02 17:32:07 +0700

    menambah peerjs dimana dapat mengirim dan menerima video dan audio stream, serta merapihkan kode dengan menambah beberapa comment

--- public/script.js
+++ public/script.js
@@ -3,26 +3,56 @@ const videoGrid = document.getElementById('video-grid');
 const myVideo = document.createElement('video');
 myVideo.muted = true;
 
-let myVideoStream;
+const myPeer = new Peer(undefined, {
+  path: '/peerjs',
+  host: '/',
+  port: '3030',
+});
+
+// Send user ID and room ID to server
+myPeer.on('open', (id) => {
+  socket.emit('join-room', ROOM_ID, id);
+});
+
+// Get user video and audio
 navigator.mediaDevices
   .getUserMedia({
     video: true,
     audio: true,
   })
   .then((stream) => {
-    myVideoStream = stream;
+    // add our own video stream without audio to html
     addVideoStream(myVideo, stream);
+    // receive other user video and audio streams
+    myPeer.on('call', (call) => {
+      call.answer(stream);
+      const video = document.createElement('video');
+      // other user video and audio stream
+      call.on('stream', (userVideoStream) => {
+        addVideoStream(video, userVideoStream); // add other user video and audio stream to html
+      });
+    });
+    // receive other user ID from server
+    socket.on('user-connected', (userId) => {
+      // call that user
+      setTimeout(() => {
+        connectToNewUser(userId, stream);
+      }, 1000);
+    });
   });
 
-socket.emit('join-room', ROOM_ID);
-socket.on('user-connected', () => {
-  connectToNewUser();
-});
-
-const connectToNewUser = () => {
-  console.log('New User');
+// Function: call other users
+const connectToNewUser = (userId, stream) => {
+  // call other users
+  const call = myPeer.call(userId, stream);
+  // send user id and stream to other users
+  const video = document.createElement('video');
+  call.on('stream', (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
 };
 
+// Function: add video and stream to html
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
   video.addEventListener('loadedmetadata', () => {
--- server.js
+++ server.js
@@ -3,14 +3,19 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const { v4: uuidv4 } = require('uuid');
+const { ExpressPeerServer } = require('peer');
 
 const socketio = require('socket.io');
 const server = http.createServer(app);
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
 const io = socketio(server);
 
 dotenv.config();
 
 //MIDDLEWARES
+app.use('/peerjs', peerServer);
 app.use(express.json());
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -25,13 +30,13 @@ app.get('/:room', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  socket.on('join-room', (roomId) => {
+  socket.on('join-room', (roomId, userId) => {
     socket.join(roomId);
-    socket.broadcast.to(roomId).emit('user-connected');
+    socket.broadcast.to(roomId).emit('user-connected', userId);
   });
 });
 
 //LISTEN
-server.listen(3000, () => {
-  console.log('listening at port 3000!');
+server.listen(3030, () => {
+  console.log('listening at port 3030!');
 });
